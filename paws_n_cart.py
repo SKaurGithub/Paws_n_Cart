f0117b2d8c0d383571747faf90d3f5ea76f235e3
SabrinaK <153448835+SKaurGithub@users.noreply.github.com>
2024-02-04 20:52:30 +0000

    Improved code by using functions
    After learning about functions in Python, I have modified the previous program.

--- paws_n_cart.py
+++ paws_n_cart.py
@@ -6,7 +6,61 @@ which allows the user to add available items, to remove items,
 to calculate the total cost and to checkout. 
 The focus for this project is user friendliness.
 
+*** Program has been improved using functions ***
+
 """
+
+#~~~~~~~~ Creating Functions ~~~~~~~~
+
+# Function to display a separator for better readability
+def separator(length = 95, symbol = "-"):
+    print(length * symbol)
+
+
+# Function to display cart:
+def cart_display():
+    count = 0
+    print("This is your shopping cart:\n")
+    for item, price in zip(items, prices):
+        count += 1
+        quant_index = items.index(item)
+        shopping_cart = f"{count}. £{price} \t\t {item:40} " \
+                        f"\t\t Quantity: {quant[quant_index]}"
+
+        print(shopping_cart)
+
+
+# Function to display stock list and 
+# their price from which the user can choose.
+def item_menu():
+    counter = 0
+    print("\nList of items available:\n")
+    for price, item in zip(stock_price, stock):
+                    counter += 1
+                    print(f"{counter}. £{price}\t\t\t{item}")
+
+
+# Function to add item:
+def add_item():
+    items.append(stock[selection])
+    items_index = items.index(stock[selection])
+    prices.append(stock_price[selection])
+    quant[items_index] += 1
+
+
+# Function to remove item:
+def remove_item():
+    items.remove(items[removed_item])
+    prices.remove(prices[removed_item])
+    quant.pop(removed_item)
+
+
+# Function to return the length of items[]:
+def length_items():
+    length = len(items)
+    separator()
+    print(f"Invalid option. Please select 1-{length}.\n")
+
 
 
 items = []
@@ -17,33 +71,27 @@ stock = ["Pedigree Wet Adult Dog Food (36 pouches)",
          "Whiskas Dry Cat Food (950g)", "Tetra Goldfish Mix (27L)"]
 stock_price = [13.50, 10.30, 45.49]
 
-print("\nWelcome to Paws n Cart!")
 
 
-# Display a menu and cart until checkout
+# Main code
+print("\n***** Welcome to Paws n Cart! *****\n")
+
+
 done = False
 while not done:
-    # print("\n")
-    print("-" * 80)
-    print("This is your shopping cart:")
+    separator()
 
 
     # Display the cart of items with their prices and the quantity
     if items:
-        for item, price in zip(items, prices):
-            quant_index = items.index(item)
-            shopping_cart = f"£{price} \t\t {item:40} " \
-                            f"\t\t Quantity: {quant[quant_index]}"
-
-            print(shopping_cart)
-
+        cart_display()
     else:
         print("Your shopping cart is empty.")
 
 
-    print("-" * 80)
+    separator()
     # Display the main menu
-    choice = input("Main Menu:\n"
+    choice = input("\nMain Menu:\n\n"
                    "1. Add an item to your cart\n"
                    "2. Remove an item from your cart\n"
                    "3. View the total cost of your cart\n"
@@ -52,22 +100,18 @@ while not done:
                    "you would like to choose (1-4):\n")
 
 
-    print("-" * 80)
+    separator()
     if choice == "1":
         # Display stock and price the user can choose from.
         while True:
-            counter = 0
-            print("Pet food:")
-            for price, item in zip(stock_price, stock):
-                counter += 1
-                print(f"{counter}. £{price}\t\t\t{item}")
+            item_menu()
 
             selection = input("\nWhat item would you like "
                               "to add to your cart (1-3)?\n"
                               "To return to the main menu, "
                               "please enter zero (0):\n")
 
-            # Using try-except for exception handling.
+
             try:
 
                 # Give the option to return to main menu,
@@ -75,36 +119,32 @@ while not done:
                 if selection == "0":
                     break
 
-            # Correct indexing
+                # Correct indexing
                 selection = int(selection) - 1
 
-            # Check if item already in cart:
-            # If it is, increase quantity and if not,
-            # add item and price to cart.
-
+                # Check if item already in cart:
+                # If it is, increase quantity and if not,
+                # add item and price to cart.
                 if stock[selection] in items:
                     items_index = items.index(stock[selection])
                     quant[items_index] += 1
                     cart_total += stock_price[selection]
-                    print(f"{stock[selection]} has been added "
+                    print(f"\n{stock[selection]} has been added "
                           f"to your cart successfully.")
                     break
 
                 else:
-                    items.append(stock[selection])
-                    items_index = items.index(stock[selection])
-                    prices.append(stock_price[selection])
-                    quant[items_index] += 1
+                    add_item()
                     cart_total += stock_price[selection]
-                    print(f"{stock[selection]} has been added "
+                    print(f"\n{stock[selection]} has been added "
                           f"to your cart successfully.")
                     break
 
             except IndexError:
-                print("-" * 80)
+                separator()
                 print("Invalid option. Please select 1-3.\n")
             except ValueError:
-                print("-" * 80)
+                separator()
                 print("Invalid option. Please select 1-3.\n")
             except Exception as e:
                 print("An unexpected error occurred:", e)
@@ -113,21 +153,13 @@ while not done:
     elif choice == "2":
         # Display the cart
         while True:
-            count = 0
-            print("This is your shopping cart:")
-            for item, price in zip(items, prices):
-                count += 1
-                quant_index = items.index(item)
-                shopping_cart = f"{count}. £{price} \t\t {item:40} " \
-                                f"\t\t Quantity: {quant[quant_index]}"
-
-                print(shopping_cart)
+            cart_display()
 
             remove = input("\nWhich item would you like to remove (1-3):\n"
                            "To return to the main menu, "
                            "please enter zero (0):\n")
 
-            # Using try-except for exception handling.
+
             try:
                 # Give the option to return to main menu,
                 # in case above selection was by mistake.
@@ -139,12 +171,11 @@ while not done:
 
                 # Find item that must be removed and
                 # check that it's in the cart.
-
                 if quant[removed_item] > 1:
                     quant[removed_item] -= 1
                     cart_total -= prices[removed_item]
 
-                    print(f"{items[removed_item]} has been removed "
+                    print(f"\n{items[removed_item]} has been removed "
                           f"from the cart successfully.")
                     break
 
@@ -152,21 +183,15 @@ while not done:
                     quant[removed_item] -= 1
                     cart_total -= prices[removed_item]
 
-                    print(f"{items[removed_item]} has been removed "
+                    print(f"\n{items[removed_item]} has been removed "
                           f"from the cart successfully.")
-                    items.remove(items[removed_item])
-                    prices.remove(prices[removed_item])
-                    quant.pop(removed_item)
+                    remove_item()
                     break
 
             except IndexError:
-                length = len(items)
-                print("-" * 80)
-                print(f"Invalid option. Please select 1-{length}.\n")
+                length_items()
             except ValueError:
-                length = len(items)
-                print("-" * 80)
-                print(f"Invalid option. Please select 1-{length}.\n")
+                length_items()
             except Exception as e:
                 print("An unexpected error occurred:", e)
 
@@ -180,7 +205,7 @@ while not done:
 
     elif choice == "4":
         # Exit from the program
-        print("Thank you for shopping with Paws n Cart!")
+        print("\nThank you for shopping with Paws n Cart!")
         done = True
 
 
